fd57151e59b9c995d12184b49a675e42b4241568
DerNiccoo <Nico.Wellermann@live.de>
2020-09-28 09:35:57 +0100

    Updates

---
+++ precache-manifest.e667608583fb3b03e49adfe615dd6d01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae09fb87980410f87f15fcab415f56b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc378369f1d18b047219",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "490311a5f5e3f371d6a3",
+    "url": "/static/js/2.daaf224b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.daaf224b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc378369f1d18b047219",
+    "url": "/static/js/main.1a42c277.chunk.js"
+  },
+  {
+    "revision": "45f0e04d90b8e8d6d798",
+    "url": "/static/js/runtime-main.33a73240.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
