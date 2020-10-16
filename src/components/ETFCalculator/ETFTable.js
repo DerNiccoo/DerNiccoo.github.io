4da17a92b966e8b79412c439a69f500a5666cefd
DerNiccoo <Nico.Wellermann@live.de>
2020-10-16 08:25:24 +0100

    documented modal, etf table, addIcon and helper file

--- src/components/ETFCalculator/ETFTable.js
+++ src/components/ETFCalculator/ETFTable.js
@@ -4,35 +4,31 @@ import {displayNumber, calculateTaxes} from "../Helper/Helper.js"
 export default function ETFTable(props) {
   
   let table = [];
-  let divMem = []; 	 
-  let mem = 0.0;     
-  let valueYE = 0.0; 
+  let dividenden_return = 0;
 
   for (let i = 1; i <= props.years; i++) {
    
-	let rate = (valueYE + props.rate) * (1.0 + props.dyn / 100.0) ** (i - 1);
+	  //let rate = (valueYE + props.rate) * (1.0 + props.dyn / 100.0) ** (i - 1.0);
+    let rate = props.rate * (1.0 + props.dyn / 100.0) ** (i - 1);
 
     let eingezahlt = parseFloat(props.start + rate * props.interval * i);
     let eingezahltNetto = (eingezahlt - (eingezahlt * (props.oc / 100.0))).toFixed(2);
 
     let growth = (eingezahltNetto * (1.0 + (props.gr + 0.0) / 100.0) ** i - eingezahltNetto).toFixed(2) /* 0.0 ist das offset) */
-    let portfolioNetto = (parseFloat(eingezahltNetto) + parseFloat(growth)).toFixed(2);
 
-	
-	divMem[i] = (eingezahltNetto * (props.dr / 100.0) * (1.0 + props.divGrowth / 100.0) ** (i - 1);
+    console.log([eingezahltNetto, dividenden_return, growth ])
+    //let portfolioNetto = parseFloat(parseFloat(eingezahltNetto) - parseFloat(eingezahltNettoLastYear) + parseFloat(growth) + parseFloat(portfolio_end_of_year)).toFixed(2);
+    let portfolioNetto = parseFloat(parseFloat(eingezahltNetto) + parseFloat(growth) + dividenden_return).toFixed(2);
+ 
+    let dividende = (portfolioNetto * (props.dr / 100.0)) * (1.0 + props.divGrowth / 100.0) ** (i - 1);
     
-	for(let o = 1; o <= i; o++)
-	{
-		let dividende += divMem[0];
-	}
+    let dividende_taxes = parseFloat(calculateTaxes(props, dividende));
+    let dividendeNetto = (dividende - dividende_taxes).toFixed(2);
 
-    
-    let divTaxes = parseFloat(calculateTaxes(props, dividende));
-    let divNetto = (dividende - divTaxes).toFixed(2);
+    let dividende_oc = (dividendeNetto * (props.oc / 100.0)).toFixed(2);
 
-    let divOC = (divNetto * (props.oc / 100.0)).toFixed(2);
-
-    valueYE = (parseFloat(divNetto) - parseFloat(divOC) + parseFloat(portfolioNetto)).toFixed(2);
+    let portfolio_end_of_year = (parseFloat(dividendeNetto) - parseFloat(dividende_oc) + parseFloat(portfolioNetto)).toFixed(2);
+    dividenden_return = parseFloat(dividendeNetto) - parseFloat(dividende_oc);
 
     /*
     Von Brutto dividende müssen noch die Kontoführungsgebüren abgezogen werden.
@@ -47,10 +43,10 @@ export default function ETFTable(props) {
         <td>{displayNumber(growth)}€</td>
         <td>{displayNumber(portfolioNetto)}€</td>
         <td>{displayNumber(dividende.toFixed(2))}€</td>
-        <td>{displayNumber(divTaxes.toFixed(2))}€</td>
-        <td>{displayNumber(divNetto)}€</td>
-        <td>{displayNumber(divOC)}€</td>
-        <td>{displayNumber(valueYE)}€</td>
+        <td>{displayNumber(dividende_taxes.toFixed(2))}€</td>
+        <td>{displayNumber(dividendeNetto)}€</td>
+        <td>{displayNumber(dividende_oc)}€</td>
+        <td>{displayNumber(portfolio_end_of_year)}€</td>
       </tr>
     );
     
