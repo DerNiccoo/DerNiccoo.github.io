07a4bbb9762211a02148b823e8fdf5ed5e859816
DerNiccoo <Nico.Wellermann@live.de>
2020-10-21 08:27:52 +0100

    code cleanup

--- src/components/ETFCalculator/ETFTable.js
+++ src/components/ETFCalculator/ETFTable.js
@@ -6,47 +6,23 @@ export default function ETFTable(props) {
   let table = [];
   let growth = [];
   let dividenden_return = 0;
-  let growthFactor = [];
-  let divMem = []; 	                                            //Zwischenspeicher der jeweiligen Dividenden pro Jahr
-
-  for(let p = 1; p <= props.years; p++)                         //Berechnung des Wachstumsfaktor der Dividende(siehe Excel-List), 
-  {
-    growthFactor.push((1 + props.divGrowth/100)**((p)-1));
-  }
-
-  growthFactor.reverse();                                       //Umdrehen der Reihenfolge
 
   for (let i = 1; i <= props.years; i++) {
    
     let rate = props.rate * (1.0 + props.dyn / 100.0) ** (i - 1);
 
-    let eingezahlt = props.start + rate * props.interval * i;                                   /* Ändern: props.start nur in der ersten eingezahlt Berechnung */
+    let eingezahlt = props.start + rate * props.interval * i;
     let eingezahltNetto = eingezahlt - (eingezahlt * (props.oc / 100.0));
 
-    let growthSum = growth.reduce((pv, cv) => pv + cv, 0);
+    let growthSum = growth.reduce((pv, cv) => pv + cv, 0); //Addiert einfach nur alle Werte in der Liste auf
 
-    growth.push((dividenden_return + eingezahltNetto + growthSum) * (props.gr / 100.0)); //eingezahltNetto * (1.0 + (props.gr + 0.0) / 100.0) ** i - eingezahltNetto /* 0.0 ist das offset) */
+    growth.push((dividenden_return + eingezahltNetto + growthSum) * (props.gr / 100.0)); 
 
     growthSum = growth.reduce((pv, cv) => pv + cv, 0);
 
     let portfolioNetto = eingezahltNetto + growthSum + dividenden_return;
  
-    if(i === 1){
-      divMem.push((eingezahltNetto + props.start) * (props.dr / 100.0));
-	  } 
-    else{
-      divMem.push((eingezahltNetto) * (props.dr / 100.0));
-	  }
-
-    let divBrutto = 0.0;// (portfolioNetto * (props.dr / 100.0)) * (1.0 + props.divGrowth / 100.0) ** (i - 1);
-
-    for(let o = 0; o < i; o++)
-    {
-      divBrutto += (eingezahltNetto)                                      //Dividende fuer das laufende Jahr
-                   * (props.dr / 100.0) + (divMem[o] * growthFactor[o]);  //Dividende für die vergangenden Jahr + divGrowth
-    }
-
-    divBrutto = portfolioNetto * (props.dr / 100.0);
+    let divBrutto = portfolioNetto * (props.dr / 100.0);
 
     let dividende_taxes = calculateTaxes(props, divBrutto);
     let dividendeNetto = divBrutto - dividende_taxes;
